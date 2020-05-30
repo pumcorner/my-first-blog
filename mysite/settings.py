87e86c594ca50e5ced72011d034b7e6f4e53950b
silin <SXL1046@student.bham.ac.uk>
2020-05-30 16:29:10 +0800

    templete 02

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y4&szd4xx7=^tuu*c1(&ab-b%=pxmn5*&%1_1e_r$$fk!90wb7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
