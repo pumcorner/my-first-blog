d37c7cdaee5c4f7aa912c18e9db341dd51ae0a21
pumcorner <51294025+pumcorner@users.noreply.github.com>
2020-08-25 11:43:24 +0800

    Update settings.py
    For css root using

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/SXL1046/sxl1046.pythonanywhere.com/static/css'
 STATICFILES_DIRS = (
      os.path.join(BASE_DIR, 'static').replace('\\', '/'),
 )
