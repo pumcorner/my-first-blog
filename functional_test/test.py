22f94d38874ebddd441cc3d6b851bfbebe30399b
silin_li <SXL1046@student.bham.ac.uk>
2020-09-01 09:23:11 +0800

    Adding bootstrap in project

--- functional_test/test.py
+++ functional_test/test.py
@@ -1,14 +1,19 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
+import os
 
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.FireFox()
+        stage_browser = os.environ.get('STAGING_SERVER')
+        if stage_server :
+            self.live_server_url = 'http://' + stage_server
 
     def tearDown():
         self.browser.quit()
@@ -17,4 +22,7 @@ class NewVisitorTest(LiveServerTestCase):
     # She clicks a title and saw words in a white squre.
     def post_words_placed_in_a_white_area(self):
         self.browser.get(live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        self.browser.find_elemnt_by_id('') 
         self.fail('Test finished')
